318154c7f4a9e51698fa968fd105e4c7e866bef0
Drew <drewlholland39@gmail.com>
2021-04-07 20:09:43 +1000

    Added command line argument parsing

--- src/dbload/dbload.java
+++ src/dbload/dbload.java
@@ -3,7 +3,24 @@ package dbload;
 public class dbload {
 
 	public static void main(String[] args) {
-		
+		int pagesize = 0;
+		String datafile = "";
+		for(int i=0; i < args.length; i++) {
+			if(args[i].equals("-p")){
+				i++;
+				pagesize = Integer.parseInt(args[i]);
+			}else {
+				datafile = args[i];
+			}
+		}
+		if(pagesize == 0) {
+			System.err.print("Pagesize argument required - use -p");
+			System.exit(0);
+		}
+		if(datafile.equals("")) {
+			System.err.print("No datafile argument provided");
+			System.exit(0);
+		}
 
 	}
 
