e5559d202471a802e0332d84f72198e92cd4d3b3
Drew <drewlholland39@gmail.com>
2021-04-12 16:32:45 +1000

    Initialised dbquery properly this time

--- dbquery.java
+++ dbquery.java
@@ -1,35 +1,53 @@
 package dbquery;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class dbquery {
 
 	public static void main(String[] args) {
-		int pagesize = 0;
-		String datafile = "";
+		String pagesize = "";
+		String query = "";
+		if(args.length < 2) {
+			System.err.print("Not enough arguments");
+			System.exit(0);
+		}
+		query = args[0];
+		pagesize = args[1];
+		String datafile = "heap." + pagesize;
 
-		for(int i=0; i < args.length; i++) {
-			if(args[i].equals("-p")){
-				i++;
-				pagesize = Integer.parseInt(args[i]);
-			}else {
-				datafile = args[i];
+		String[] queries = new String[2];
+		String sensorQuery = query.substring(0, query.length() - 20);
+		
+		String dateQuery = query.substring(query.length() - 20);
+		dateQuery = dateQuery.substring(0, 10) + " " + dateQuery.substring(10, 18) + " " + dateQuery.substring(18, 20);
+		
+		BigInteger bigInt = BigInteger.valueOf(Integer.parseInt(sensorQuery));
+		byte[] sensorBytes = bigInt.toByteArray();
+		for(byte b : sensorBytes) {
+			String byteString = Integer.toBinaryString(b);
+			while(byteString.length() < 8) {
+					byteString = "0" + byteString;
 			}
+			queries[0] = byteString;
 		}
-		if(pagesize == 0) {
-			System.err.print("Pagesize argument required - use -p");
-			System.exit(0);
+		
+		queries[1] = "";
+		byte[] dateBytes = dateQuery.getBytes();
+		for(byte b : dateBytes) {
+			String byteString = Integer.toBinaryString(b);
+			while(byteString.length() < 8) {
+				byteString = "0" + byteString;
+			}
+			if(!queries[1].equals("")) {
+				queries[1] += " ";
+			}
+			queries[1] += byteString;
 		}
-		if(datafile.equals("")) {
-			System.err.print("No datafile argument provided");
-			System.exit(0);
-		}
-		String[] datafileSub = datafile.split(".");
-		if(pagesize != Integer.parseInt(datafileSub[-1])) {
-			System.err.print("Pagesize must be the same as that of the file provided");
-			System.exit(0);
-		}
+		
+		Scanner s = new Scanner(datafile);
+		s.close();
 
 	}
 
